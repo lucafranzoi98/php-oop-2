8bf5d2cb034d8cb5c47e6fb2e98a38be8295414d
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-10-20 15:16:44 +0200

    add classes, datas and layout

--- index.php
+++ index.php
@@ -1,11 +1,48 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+define('ROOT', __DIR__);
+define('DS', DIRECTORY_SEPARATOR);
+require_once ROOT . DS . 'classes' . DS . 'item.php';
+require_once ROOT . DS . 'models' . DS . 'items.php';
+require_once ROOT . DS . 'db.php';
+
+array_push($items, $biscuits, $ball, $litter, $dispenser, $rope, $bowl, $pillow, $scratcher)
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
    <title>OOP 2</title>
 </head>
-<body>
-   
+<body class="bg-dark text-white">
+   <h1 class="text-center my-3">Pet Store</h1>
+
+   <div class="container mb-5">
+      <div class="row row-cols-4 gy-3">
+         <?php foreach ($items as $item) : ?>
+         
+         <div class="col">
+            <div class="card h-100">
+               <div style="height: 15rem" class="d-flex justify-content-center align-items-center">
+                  <img src="./img/<?= $item->getImg() ?>" class="card-img-top w-50">
+               </div>               
+               <div class="card-body">
+                  <img src="./img/<?= $item->getCategory() ?>.png" style="width: 1rem">
+                  <h5 class="card-title"><?= $item->getName() ?></h5>
+                   <h6 class="card-subtitle mb-2 text-muted "><?= $item->getPrice() ?> $</h6>
+                  <p class="card-text"><?= $item->getDetails() ?></p>
+                  <h6 class="card-subtitle mb-2 text-muted "><?= $item->getType() ?></h6>
+               </div>
+            </div>
+         </div>
+
+         <?php endforeach ?>
+      </div>
+   </div>
 </body>
 </html>
---
+++ models/items.php
@@ -0,0 +1,10 @@
+<?php
+
+$biscuits = new Item ("Biscuits Name", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laudantium", "biscuits.jpg", 10.99, "Food", "Dog");
+$ball = new Item ("Ball Name", "nobis quasi animi ducimus magni, quos adipisci. Asperiores, dolores", "ball.jpg", 15.99, "Toy", "Dog");
+$litter = new Item ("Litter Box Name", "Corrupti ducimus cupiditate nobis quod expedita at quam excepturi ab", "litter.jpg", 32.99, "Utility", "Cat");
+$dispenser = new Item ("Dispenser Name", "id assumenda mollitia eum accusantium voluptatem molestiae vero! Ipsam", "dispenser.jpg", 89.99, "Utility", "Cat");
+$rope = new Item ("Rope Name", "libero debitis aut mollitia suscipit autem repudiandae ratione error", "rope.jpg", 9.99, "Toy", "Dog");
+$bowl = new Item ("Bowl Name", "molestiae accusamus id itaque quisquam necessitatibus omnis doloremque", "bowl.jpg", 19.99, "Utility", "Cat");
+$pillow = new Item ("Pillow Name", "dolorem illum, asperiores quasi recusandae deleniti nihil laborum eius", "pillow.jpg", 5.99, "Utility", "Dog");
+$scratcher = new Item ("Scratcher Name", "assumenda! Facere corporis nam soluta praesentium saepe delectus", "scratcher.jpg", 15.99, "Utility", "Cat");
